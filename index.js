70225c744b3b1ba8214f81856aa713804bb836ab
Rafael Dias <74469120+rafaeldias12@users.noreply.github.com>
2021-03-03 20:42:03 -0300

    Adiciando o Hello Word.

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+    return res.send('Hello World =D')
+});
+
 app.listen(3000, () => {
     console.log('Servidor rodando na porta 3000')
 });
-
