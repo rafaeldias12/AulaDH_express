774d12df134a8d4e2c3104fd8f7531f393d91c14
Rafael Dias <74469120+rafaeldias12@users.noreply.github.com>
2021-03-03 21:46:53 -0300

    Finalizando a Aula de Hoje.

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    return res.send('Hello World =D')
+let listaUsuario = [
+    "rafael",
+    "ivens",
+    "marcia",
+    "samuka"
+]
+//Query Params
+// Ex: localhost:3000/usuario?id=1
+app.get('/usuarios', (req, res) => {
+    const {nome} = req.query;
+    let listaRetorno = listaUsuario.filter(i => i.includes(nome || ''));
+    return res.json(listaRetorno)
+});
+
+app.get('/:usuario', (req, res) => {
+    const {usuario} = req.params;
+    return res.send(`Hello word ${usuario}!`)
 });
 
 app.listen(3000, () => {
